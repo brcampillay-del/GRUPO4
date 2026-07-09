d3f780c3134bc839ec7cd4637240c6869427461e
LENOVO <br.campillay@duocuc.cl>
2026-07-09 11:53:25 -0400

    examen

--- proyectocompleto/api-gateway/src/main/java/cl/duoc/api_gateway/controller/TestController.java
+++ proyectocompleto/api-gateway/src/main/java/cl/duoc/api_gateway/controller/TestController.java
@@ -4,12 +4,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/gateway")
 public class TestController {
 
     @GetMapping("/status")
-    public String status(){
-        return "API Gateway Veranum operativo al 100%";
+    public Map<String, String> obtenerEstado() {
+        Map<String, String> response = new HashMap<>();
+        response.put("nombre", "api-gateway");
+        response.put("estado", "operativo");
+        return response;
     }
 }
